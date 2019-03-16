23c182bd8fe8fda20d2ad8d5b3bd91eef2938af7
mpppk <niboshiporipori@gmail.com>
2019-03-16 14:37:38 +0900

    Add GenerateErrorWrappersFromProgram, WriteAstFile

--- cmd/mustify.go
+++ cmd/mustify.go
@@ -1,15 +1,10 @@
 package cmd
 
 import (
-	"go/ast"
-	"go/format"
-	"go/token"
-	"os"
 	"path/filepath"
 
 	"github.com/mpppk/goofy/lib"
 
-	goofyast "github.com/mpppk/goofy/ast"
 	"github.com/spf13/cobra"
 )
 
@@ -20,78 +15,20 @@ var mustifyCmd = &cobra.Command{
 	Short: "A brief description of your command",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		absFilePath, err := filepath.Abs(*filePath)
-		if err != nil {
-			panic(err)
-		}
-
 		if *outFilePath == "" {
 			base := filepath.Base(*filePath)
 			o := filepath.Join(filepath.Dir(*filePath), "must-"+base)
 			outFilePath = &o
 		}
 
-		absOutFilePath, err := filepath.Abs(*outFilePath)
+		file, newDecls, err := lib.GenerateErrorWrappersFromProgram(*filePath)
 		if err != nil {
 			panic(err)
 		}
+		file.Decls = newDecls
 
-		prog, err := goofyast.NewProgram(*filePath)
-		if err != nil {
+		if err := lib.WriteAstFile(*outFilePath, file); err != nil {
 			panic(err)
-		}
-
-		for _, pkg := range prog.Created {
-			for i, file := range pkg.Files {
-				currentFilePath := prog.Fset.File(pkg.Files[i].Pos()).Name()
-				absCurrentFilePath, err := filepath.Abs(currentFilePath)
-				if err != nil {
-					panic(err)
-				}
-
-				if absFilePath != absCurrentFilePath {
-					continue
-				}
-
-				var newDecls []ast.Decl
-				for _, decl := range file.Decls {
-					if importDecl, ok := lib.DeclToImportDecl(decl); ok {
-						newDecls = append(newDecls, importDecl)
-						continue
-					}
-
-					funcDecl, ok := decl.(*ast.FuncDecl)
-					if !ok {
-						panic("unknown decl in " + currentFilePath)
-					}
-
-					if !ast.IsExported(funcDecl.Name.Name) {
-						continue
-					}
-
-					newDecl, ok := goofyast.GenerateErrorFuncWrapper(pkg, funcDecl)
-					//newDecl, ok := goofyast.ConvertErrorFuncToMustFunc(prog, pkg, funcDecl)
-					if !ok {
-						continue
-					}
-					newDecls = append(newDecls, newDecl)
-				}
-				file.Decls = newDecls
-
-				f, err := os.Create(absOutFilePath)
-				if err != nil {
-					panic(err)
-				}
-
-				defer func() {
-					if err := f.Close(); err != nil {
-						panic(err)
-					}
-				}()
-				if err := format.Node(f, token.NewFileSet(), file); err != nil {
-					panic(err)
-				}
-			}
 		}
 	},
 }
--- lib/mustify.go
+++ lib/mustify.go
@@ -2,14 +2,53 @@ package lib
 
 import (
 	"go/ast"
+	"go/format"
 	"go/token"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
 
 	"golang.org/x/tools/go/loader"
 
 	goofyast "github.com/mpppk/goofy/ast"
 )
 
-func DeclToImportDecl(decl ast.Decl) (*ast.GenDecl, bool) {
+func GenerateErrorWrappersFromProgram(filePath string) (*ast.File, []ast.Decl, error) {
+	absFilePath, err := filepath.Abs(filePath)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to get abs file path")
+	}
+
+	prog, err := goofyast.NewProgram(filePath)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to load program file")
+	}
+
+	pkg, file, ok := findPkgAndFileFromProgram(prog, absFilePath)
+	if !ok {
+		return nil, nil, errors.New("file not found: " + filePath)
+	}
+
+	var newDecls []ast.Decl
+	importDecls := extractImportDeclsFromDecls(file.Decls)
+	newDecls = append(newDecls, importDecls...)
+	exportedFuncDecls := extractExportedFuncDeclsFromDecls(file.Decls)
+	errorWrappers := funcDeclsToErrorFuncWrappers(exportedFuncDecls, pkg)
+	newDecls = append(newDecls, errorWrappers...)
+	return file, newDecls, nil
+}
+
+func extractImportDeclsFromDecls(decls []ast.Decl) (importDecls []ast.Decl) {
+	for _, decl := range decls {
+		if importDecl, ok := declToImportDecl(decl); ok {
+			importDecls = append(importDecls, importDecl)
+		}
+	}
+	return
+}
+
+func declToImportDecl(decl ast.Decl) (*ast.GenDecl, bool) {
 	if genDecl, ok := decl.(*ast.GenDecl); ok {
 		if genDecl.Tok == token.IMPORT {
 			return genDecl, true
@@ -18,18 +57,60 @@ func DeclToImportDecl(decl ast.Decl) (*ast.GenDecl, bool) {
 	return nil, false
 }
 
-func GenerateErrorFuncWrappers(funcDecls []*ast.FuncDecl, pkg *loader.PackageInfo) (newDecls []ast.Decl) {
-	for _, funcDecl := range funcDecls {
-		if !ast.IsExported(funcDecl.Name.Name) {
-			continue
+func extractExportedFuncDeclsFromDecls(decls []ast.Decl) (funcDecls []*ast.FuncDecl) {
+	for _, decl := range decls {
+		if funcDecl, ok := decl.(*ast.FuncDecl); ok {
+			if ast.IsExported(funcDecl.Name.Name) {
+				funcDecls = append(funcDecls, funcDecl)
+			}
 		}
-
-		newDecl, ok := goofyast.GenerateErrorFuncWrapper(pkg, funcDecl)
-		//newDecl, ok := goofyast.ConvertErrorFuncToMustFunc(prog, pkg, funcDecl)
-		if !ok {
-			continue
-		}
-		newDecls = append(newDecls, newDecl)
 	}
 	return
+}
+
+func funcDeclsToErrorFuncWrappers(funcDecls []*ast.FuncDecl, pkg *loader.PackageInfo) (newDecls []ast.Decl) {
+	for _, funcDecl := range funcDecls {
+		//newDecl, ok := goofyast.ConvertErrorFuncToMustFunc(prog, pkg, funcDecl)
+		if newDecl, ok := goofyast.GenerateErrorFuncWrapper(pkg, funcDecl); ok {
+			newDecls = append(newDecls, newDecl)
+		}
+	}
+	return
+}
+
+func findPkgAndFileFromProgram(prog *loader.Program, targetAbsFilePath string) (*loader.PackageInfo, *ast.File, bool) {
+	for _, pkg := range prog.Created {
+		for _, file := range pkg.Files {
+			currentFilePath := prog.Fset.File(file.Pos()).Name()
+			if absCurrentFilePath, err := filepath.Abs(currentFilePath); err == nil {
+				if targetAbsFilePath == absCurrentFilePath {
+					return pkg, file, true
+				}
+			}
+
+		}
+	}
+	return nil, nil, false
+}
+
+func WriteAstFile(filePath string, file *ast.File) error {
+	absFilePath, err := filepath.Abs(filePath)
+	if err != nil {
+		return errors.Wrap(err, "failed to get abs file path: "+absFilePath)
+	}
+
+	f, err := os.Create(absFilePath)
+	if err != nil {
+		return errors.Wrap(err, "failed to create file: "+absFilePath)
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	if err := format.Node(f, token.NewFileSet(), file); err != nil {
+		return errors.Wrap(err, "failed to write ast file to  "+absFilePath)
+	}
+	return nil
 }
